3e83b3dbdc12bfc5c043afd7f71995d1aaf65c56
Chirag Kular <ck4957@rit.edu>
2018-03-20 00:20:04 -0500

    Get Album Data

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,9 +7,14 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor(private _product: ProductService) { }
+  albumInfo;
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1)
+                  .subscribe((response) => 
+                  this.albumInfo = response);
   }
+
 
 }
